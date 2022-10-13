d730729f9e6394ba5a9e891c7e4f813506216ebc
Chris May <chris@may.org>
2022-10-13 14:32:02 -0400

    AutoBadge cog: Correct class case, format file

--- src/cogs/autobadge.py
+++ src/cogs/autobadge.py
@@ -1,25 +1,24 @@
-import os
-
 from discord.ext import commands
 
 
-class autobadge(commands.Cog):
+class AutoBadge(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    @commands.command(name='whos_here', help='Responds with list of people here in mainstage', pass_context=True)
+
+    @commands.command(
+        name='whos_here', help='Responds with list of people here in mainstage',
+        pass_context=True
+    )
     async def whos_here(self, ctx):
-        channel = self.bot.get_channel(691841117775462445) #gets the channel you want to get the list from
+        channel = self.bot.get_channel(
+            691841117775462445
+        )  # gets the channel you want to get the list from
+        members = channel.members  # finds members connected to the channel
 
-        members = channel.members #finds members connected to the channel
-
-        mem = [] #(list)
-        for member in members:
-            mem.append(member.name)
-
-        print(mem) #print info
+        mem = [member.name for member in members]
+        print(mem)  # print info
         await ctx.send(mem)
 
 
 async def setup(bot):
-    await bot.add_cog(autobadge(bot))
+    await bot.add_cog(AutoBadge(bot))
