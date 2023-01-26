bd8a137adeecfd104307635de1d5857e800530e4
Richard Blanchette <richardphi1618@gmail.com>
2023-01-26 16:14:37 -0500

    refactor: add pre-commit and style checks. put autobadge in timeout since not acting right. update to requirements

--- src/cogs/Fun.py
+++ src/cogs/Fun.py
@@ -10,14 +10,15 @@ from discord.ext import commands
 
 logger = logging.getLogger(__name__)
 
+
 class ForFun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.resource_folder = "cogs/fun/"
-    
+
     fun = SlashCommandGroup("fun", "for the lolz")
-    
-    @fun.command(description='Responds with a random quote from Futurama')
+
+    @fun.command(description="Responds with a random quote from Futurama")
     async def futurama(self, ctx: discord.ApplicationContext):
         curpath = os.path.abspath(os.curdir)
         filename = f"{self.resource_folder}futurama.json"
@@ -28,10 +29,10 @@ class ForFun(commands.Cog):
 
         response = random.choice(futurama_quotes)
 
-        logger.info(f'output:\n{response}')
+        logger.info(f"output:\n{response}")
         await ctx.respond(response)
 
-    @fun.command(description='Responds with zen of python by Tim Peters')
+    @fun.command(description="Responds with zen of python by Tim Peters")
     async def zen(self, ctx: discord.ApplicationContext):
         curpath = os.path.abspath(os.curdir)
         filename = f"{self.resource_folder}zen_of_python.md"
@@ -40,40 +41,30 @@ class ForFun(commands.Cog):
         response = f.read()
         await ctx.respond(response)
 
-    @fun.command()
-    async def suggest(self, ctx, *, args):
-        logger.info(f'function: suggest / requester: {str(ctx.message.author).split("#")[0]}')
-
-        await ctx.message.delete()
-        embed = discord.Embed(description=args, timestamp=ctx.message.created_at)
-        embed.set_author(name=f"Suggestion by {ctx.message.author.display_name}", icon_url=ctx.message.author.avatar_url)
-        msg = await ctx.respond(embed=embed)
-        await msg.add_reaction("👍")	
-        await msg.add_reaction("👎")
-    
-    @fun.command(
-      name='roll_dice', 
-      description='Simulates rolling dice.')
+    @fun.command(name="roll_dice", description="Simulates rolling dice.")
     @option(
         name="number_of_dice",
         description="number of dice",
         min_value=1,
         max_value=99,
         required=True,
-        )
+    )
     @option(
         name="number_of_sides",
         description="number of sides",
         min_value=1,
         max_value=99,
         required=True,
-        )
-    async def _roll(self, ctx: discord.ApplicationContext, number_of_dice: int, number_of_sides: int):
-      dice = [
-          str(random.choice(range(1, number_of_sides + 1)))
-          for _ in range(number_of_dice)
-      ]
-      await ctx.respond(', '.join(dice))
+    )
+    async def _roll(
+        self, ctx: discord.ApplicationContext, number_of_dice: int, number_of_sides: int
+    ):
+        dice = [
+            str(random.choice(range(1, number_of_sides + 1)))
+            for _ in range(number_of_dice)
+        ]
+        await ctx.respond(", ".join(dice))
+
 
 def setup(bot):
     bot.add_cog(ForFun(bot))
--- src/cogs/Utilities.py
+++ src/cogs/Utilities.py
@@ -1,5 +1,4 @@
 import logging
-import os
 
 import discord
 import requests
@@ -9,8 +8,9 @@ from discord.ext import commands
 
 logger = logging.getLogger(__name__)
 
+
 class utilities(commands.Cog):
-    util = SlashCommandGroup("util", "utility commands")   
+    util = SlashCommandGroup("util", "utility commands")
 
     def __init__(self, bot):
         self.bot = bot
@@ -22,80 +22,110 @@ class utilities(commands.Cog):
     async def admin_only(self, ctx: discord.ApplicationContext):
         await ctx.respond(f"Hello {ctx.author}, you are an administrator.")
 
-    def latency_check(self): # Alias for the Z.ping command
-        return(discord.Embed(
+    def latency_check(self):  # Alias for the Z.ping command
+        return discord.Embed(
             title="Pong.",
             description=f"Bot's current ping is **{round(self.bot.latency * 1000)}ms**",
-            color=discord.Color.green()))
-    
-    @util.command(name='latency', help='measures the latency of the bot')
+            color=discord.Color.green(),
+        )
+
+    @util.command(name="latency", help="measures the latency of the bot")
     async def latency(self, ctx: discord.ApplicationContext):
         await ctx.respond(embed=self.latency_check())
 
-    @util.command(name='google', help='creates a lmgtfy link')
+    @util.command(name="google", help="creates a lmgtfy link")
     async def google(self, ctx, *, args):
-        await ctx.respond(f"Here, allow me to google that one for you:\nhttps://letmegooglethat.com/?q={args.replace(' ', '+')}") #,reference=ctx.message
-    
-    @util.command(help='searches for pip package')
+        await ctx.respond(
+            f"Here, allow me to google that one for you:\nhttps://letmegooglethat.com/?q={args.replace(' ', '+')}"
+        )  # ,reference=ctx.message
+
+    @util.command(help="searches for pip package")
     @option(
         name="package",
         input_type=str,
         description="what package do you want to search for",
         required=True,
-        )
-    async def pipsearch(self, ctx: discord.ApplicationContext, package:str):        
+    )
+    async def pipsearch(self, ctx: discord.ApplicationContext, package: str):
         if not package:
-            await ctx.respond(embed=discord.Embed(title="Traceback (most recent call): \"~/ur_brain\"", description="Invalid pacakge name!", colour=discord.Colour.red()))
+            await ctx.respond(
+                embed=discord.Embed(
+                    title='Traceback (most recent call): "~/ur_brain"',
+                    description="Invalid pacakge name!",
+                    colour=discord.Colour.red(),
+                )
+            )
         else:
             data = requests.get(f"https://pypi.org/pypi/{package}/json").json()
-            
-            embed=discord.Embed(
+
+            embed = discord.Embed(
                 title=f"Searched {package}",
                 description=f"[Project URL]({data['info']['package_url']})",
                 colour=discord.Colour.green(),
             )
 
-            embed.add_field(name=f"{data['info']['name']}-{data['info']['version']}", value=f"{data['info']['summary']}")
+            embed.add_field(
+                name=f"{data['info']['name']}-{data['info']['version']}",
+                value=f"{data['info']['summary']}",
+            )
             await ctx.respond(embed=embed)
-    
-    @util.command(help='returns embed of github repo')
+
+    @util.command(help="returns embed of github repo")
     @option(
         name="endpoint",
         input_type=str,
         description="what repo do you want to search for",
         required=True,
-        )
+    )
     async def github(self, ctx: discord.ApplicationContext, endpoint: str):
         try:
             info = requests.get(f"https://api.github.com/repos/{endpoint}").json()
-            contrib_info = requests.get(f"https://api.github.com/repos/{endpoint}/contributors").json()
+            contrib_info = requests.get(
+                f"https://api.github.com/repos/{endpoint}/contributors"
+            ).json()
             embed = discord.Embed(
                 title=info["name"],
                 description=f"[Repository Link]({info['html_url']})",
-                colour=discord.Colour.green())
-        
-            embed.add_field(name="Owner",value=info["owner"]["login"])
-            embed.add_field(name="Language",value=info["language"])
-            embed.add_field(name="Stars",value=info["stargazers_count"])
-            embed.add_field(name="Forks",value=info["forks"])
-            embed.add_field(name="License",value=info["license"]["name"] if info["license"] is not None else "None")
-            embed.add_field(name="Open Issues",value=info["open_issues"])
-            embed.add_field(name="Contributors",value="\n".join([contribs["login"] for contribs in contrib_info]))
+                colour=discord.Colour.green(),
+            )
+
+            embed.add_field(name="Owner", value=info["owner"]["login"])
+            embed.add_field(name="Language", value=info["language"])
+            embed.add_field(name="Stars", value=info["stargazers_count"])
+            embed.add_field(name="Forks", value=info["forks"])
+            embed.add_field(
+                name="License",
+                value=info["license"]["name"]
+                if info["license"] is not None
+                else "None",
+            )
+            embed.add_field(name="Open Issues", value=info["open_issues"])
+            embed.add_field(
+                name="Contributors",
+                value="\n".join([contribs["login"] for contribs in contrib_info]),
+            )
             embed.set_thumbnail(url=info["owner"]["avatar_url"])
-        except:
+        except ValueError:
             embed = discord.Embed(
                 title="Oops",
                 description="Repository does not exist.",
-                colour=discord.Colour.red())
+                colour=discord.Colour.red(),
+            )
 
         await ctx.respond(embed=embed)
-    
+
     @util.command(name="userinfo", description="Gets info about a user.")
-    async def user_info(self, ctx: discord.ApplicationContext, user: discord.Member = None):
-        user = user or ctx.author  # If no user is provided it'll use the author of the message
+    async def user_info(
+        self, ctx: discord.ApplicationContext, user: discord.Member = None
+    ):
+        user = (
+            user or ctx.author
+        )  # If no user is provided it'll use the author of the message
         embed = discord.Embed(
             fields=[
-                discord.EmbedField(name="ID", value=str(user.id), inline=False),  # User ID
+                discord.EmbedField(
+                    name="ID", value=str(user.id), inline=False
+                ),  # User ID
                 discord.EmbedField(
                     name="Created",
                     value=discord.utils.format_dt(user.created_at, "F"),
@@ -120,6 +150,20 @@ class utilities(commands.Cog):
 
         await ctx.respond(embeds=[embed])  # Sends the embed
 
+    @util.command(name="suggestion", description="facilitates making a suggestion")
+    @option(
+        name="suggestion",
+        description="This is the suggestion you are asking feedback for",
+        required=True,
+    )
+    async def suggest(self, ctx: discord.ApplicationContext, suggestion: str):
+        logger.info(f"function: suggest / requester: {str(ctx.author)}")
+        print(suggestion)
+        embed = discord.Embed(description=suggestion)
+        embed.set_author(name=f"Suggestion by {ctx.author.name}")
+        msg = await ctx.send(embed=embed)
+        await msg.add_reaction("👍")
+        await msg.add_reaction("👎")
 
 
 def setup(bot):
